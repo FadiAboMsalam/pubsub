ee6e3d615b45ff9f8d22bf71f2fb86d414027ecb
FadiAboMsalam <abumsalam.1090174@gmail.com>
2016-03-02 01:56:13 +0200

    final pubsub

--- models/badges.js
+++ models/badges.js
@@ -1,35 +1,39 @@
 'use strict';
-var redis=require('../lib/redis');
-var broadcast=require('../lib/broadcast');
+var redis = require('../lib/redis');
+var broadcast = require('../lib/broadcast');
 
-function save (badges,callback){
-	if(badges.length==0){console.log("runs/");callback(null,null);}
-	else {
-		var badge=badges.pop();
-		
-		redis.lpush("badges",JSON.stringify(badge),function(err,data){
-			if(err){callback(err,null);console.log("error",err);}
-		});
-		save(badges,callback);
-	}
-}
-function trim(){
-	redis.ltrim("badges",0,9);
-}
-function sends(badges,callback){
-	badges.forEach(broadcast.send);
-	//	badges.forEach(function(value){
-	// 	broadcast.send(value);
-	// });
-	callback(null,null);
-}
-function get(callback){
-	redis.lrange("badges",0,-1,function(err,badges){
-		callback(err,badges.map(JSON.parse));
-	})
+function save(badges, callback) {
+    if (badges.length == 0) { console.log("runs/");
+        callback(null, null); } else {
+        var badge = badges.pop();
+
+        redis.lpush("badges", JSON.stringify(badge), function(err, data) {
+            if (err) { callback(err, null);
+                console.log("error", err); }
+        });
+        save(badges, callback);
+    }
 }
 
-exports.get=get;
-exports.save=save;
-exports.trim=trim;
-exports.sends=sends;
+function trim() {
+    redis.ltrim("badges", 0, 9);
+}
+
+function sends(badges, callback) {
+    badges.forEach(broadcast.send);
+    //	badges.forEach(function(value){
+    // 	broadcast.send(value);
+    // });
+    callback(null, null);
+}
+
+function get(callback) {
+    redis.lrange("badges", 0, -1, function(err, badges) {
+        callback(err, badges.map(JSON.parse));
+    })
+}
+
+exports.get = get;
+exports.save = save;
+exports.trim = trim;
+exports.sends = sends;
